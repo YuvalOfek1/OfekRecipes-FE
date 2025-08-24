067a7af5c443ffb0853e6ba3c87f87579dd33abd
YuvalOfek1 <yuvalofek97@gmail.com>
2025-08-24 18:20:50 +0300

    try another way

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? (process.env.ASSET_BASE || '/') : '/',
   plugins: [react()],
   server: {
     proxy: {
